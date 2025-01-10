4f92728804da0e8ebdb9c18865dcf569754890fe
Faishal <musthafafaishal@gmail.com>
2025-01-11 01:02:14 +0700

    chore(layout): add speed insight tag from vercel

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "Faishal Dev: Crafting code like threads of art",
-  description: "Where innovation meets artistry, weaving digital dreams into reality through clean code and creative solutions",
+  title: 'Faishal Dev: Crafting code like threads of art',
+  description:
+    'Where innovation meets artistry, weaving digital dreams into reality through clean code and creative solutions',
 };
 
 export default function RootLayout({ children }) {
@@ -23,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
