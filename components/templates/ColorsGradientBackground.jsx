a64bbdce7f5ab85ccb8340cf923f911ff0c1faa9
Faishal <musthafafaishal@gmail.com>
2025-01-11 00:44:23 +0700

    feat(templates): create colors gradient background component

---
+++ components/templates/ColorsGradientBackground.jsx
@@ -0,0 +1,27 @@
+import Noise from '../../assets/images/noise.png';
+import LinePattern from '../../assets/images/line-pattern.svg';
+
+export default function ColorsGradientBackground() {
+  return (
+    <div
+      className="
+        bg-gradient-to-r
+      from-red-500
+      via-green-500
+      to-purple-500
+        relative
+        w-full
+        h-full"
+    >
+      <div
+        className="absolute w-full h-full opacity-25"
+        style={{ backgroundImage: `url(${Noise.src})` }}
+      />
+      <div
+        className="absolute w-full h-full"
+        style={{ backgroundImage: `url(${LinePattern.src})` }}
+      />
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#101012]" />
+    </div>
+  );
+}
